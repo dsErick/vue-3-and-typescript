b534871c30a98ceab59e23213d04f04251e165d8
Erick Silva <ericksilvablue@gmail.com>
2021-03-03 11:04:51 -0300

    Custom Types

---
+++ src/assets/ts/customTypes.ts
@@ -0,0 +1,23 @@
+// Union operator |
+
+type ButtonType = "primary" | "secondary" | "success" | "danger";
+
+type ComicUniverse = "Marvel" | "DC";
+
+interface Hero {
+  name: string;
+  age: number;
+  activeAvenger: boolean;
+  powers: string[];
+  universe: ComicUniverse;
+};
+
+
+const buttonStyle: ButtonType = "primary";
+const person: Hero = {
+  name: "Tony Stark",
+  age: 31,
+  activeAvenger: false,
+  powers: ["Rich", "Full-Tech Armor"],
+  universe: "Marvel"
+}
--- src/assets/ts/typeFundamentals.ts
+++ src/assets/ts/typeFundamentals.ts
@@ -3,34 +3,34 @@ const lastName = "Silva";
 const age = 20;
 const isMarried = false;
 const frameworksAndLanguages: string[] = [
-    "JavaScript",
-    "Vue",
-    "PHP",
-    "Laravel"
+  "JavaScript",
+  "Vue",
+  "PHP",
+  "Laravel"
 ];
 
 const fullName = (firstName: string, lastName: string): string => {
-    return `${firstName} ${lastName}`;
+  return `${firstName} ${lastName}`;
 };
 
 const heroe: {
-    name: string;
-    age: number;
-    activeAvenger: boolean;
-    powers: string[];
+  name: string;
+  age: number;
+  activeAvenger: boolean;
+  powers: string[];
 } = {
-    name: "Peeter Parker",
-    age: 20,
-    activeAvenger: true,
-    powers: ["wall-crawl", "spider-sense"]
+  name: "Peeter Parker",
+  age: 20,
+  activeAvenger: true,
+  powers: ["wall-crawl", "spider-sense"]
 };
 
 console.log(
-    firstName,
-    lastName,
-    age,
-    isMarried,
-    frameworksAndLanguages,
-    heroe,
-    fullName(firstName, lastName)
+  firstName,
+  lastName,
+  age,
+  isMarried,
+  frameworksAndLanguages,
+  heroe,
+  fullName(firstName, lastName)
 );
